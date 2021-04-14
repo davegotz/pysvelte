4b7cd607a9e48b3340a43ff97eea51c752550e77
David Gotz <gotz@unc.edu>
2021-04-14 09:53:19 -0400

    Added login flow to the sample app.

---
+++ webapp/src/Login.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import {
+    Form,
+    TextInput,
+    PasswordInput,
+    Button
+  } from "carbon-components-svelte";
+
+  const dispatch = createEventDispatcher();
+
+</script>
+
+<p>Please login with your UNC Onyen.</p>
+<Form on:submit={(e,d) => {dispatch('submit', {'username': e.target.username.value, 'password': e.target.password.value});}}>
+  <TextInput
+    required
+    name="username"
+    id="username"
+    labelText="User name"
+    autocomplete="username"
+    placeholder="Enter user name..."
+  />
+  <PasswordInput
+    required
+    id="password"
+    type="password"
+    labelText="Password"
+    autocomplete="current-password"
+    placeholder="Enter password..."
+  />
+  <Button type="submit">Submit</Button>
+</Form>
